e9ea9b11596ec21591a4fb8d953a4ae97e698901
Daniel Spiewak <djspiewak@gmail.com>
2017-03-22 12:23:21 -0600

    Added some more functions

--- src/main/scala/cont/Task.scala
+++ src/main/scala/cont/Task.scala
@@ -8,6 +8,12 @@ import java.util.concurrent.ExecutorService
 // TODO exceptions???
 object Task {
 
+  def now[A](a: A): Task[A] = Cont.pure(a)
+
+  def delay[A](body: => A): Task[A] = Cont(_(body))
+
+  def async[A](f: (A => IO[Unit]) => IO[Unit]): Task[A] = Cont(f)
+
   def flatMap[A, B](t: Task[A])(f: A => Task[B]): Task[B] = t.flatMap(f)
 
   def pzip[A, B](left: Task[A], right: Task[B])(implicit E: ExecutorService): Task[(A, B)] = {
